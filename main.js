28bbe2b41fa735d59660bb4758421a80df558006
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2016-12-04 02:41:18 -0800

    got button working

--- main.js
+++ main.js
@@ -2,16 +2,25 @@ var Cylon = require('cylon');
 var mraa = require('mraa');
 Cylon.robot(
   {connections: {
-    edison: {adaptor: 'intel-iot'},
-    arduino: {adaptor: 'firmata', port: '/dev/ttyACM0'}
+    edison: {adaptor: 'intel-iot'}
     },
 
     devices: {
-      led: {driver: 'led', pin: 13},
-      button: {driver: 'button', pin: 6}
+      led: {
+        driver: 'led',
+        pin: 13,
+        connection: 'edison'
+      },
+      button: {
+        driver: 'button',
+        pin: 6,
+        connection: 'edison'
+      }
     },
 
     work: function(my) {
+      // my.connections;
+      // edison: []
       every((1).second(), my.led.toggle);
       my.button.on('push', function() {
         console.log("Button pushed!");
